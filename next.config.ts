962e01e400542d48ef40b786261dcf4222c5e2a1
ishikawatakashi-create <ishikawa.takashi@in-iru.com>
2026-03-30 22:24:51 +0900

    fix: include pdf font assets in server trace

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,12 @@ const nextConfig: NextConfig = {
     "@prisma/client",
     ".prisma/client",
   ],
+  outputFileTracingIncludes: {
+    "/api/documents/\\[id\\]/pdf": [
+      "./node_modules/@fontsource/noto-sans-jp/files/noto-sans-jp-japanese-400-normal.woff2",
+      "./node_modules/@fontsource/noto-sans-jp/files/noto-sans-jp-japanese-700-normal.woff2",
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: false,
   },
